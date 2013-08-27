7397c01c7f6ababd3efb538b6d60b3985d4f515a
Postmodern <postmodern.mod3@gmail.com>
2013-08-26 18:00:25 -0700

    Updated the integration specs given the new ruby-advisory-db.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -23,7 +23,23 @@ describe "CLI" do
       expect = %{
 Name: actionpack
 Version: 3.2.10
-Advisory: CVE-2013-0156
+Advisory: OSVDB-91452
+Criticality: Medium
+URL: http://www.osvdb.org/show/osvdb/91452
+Title: XSS vulnerability in sanitize_css in Action Pack
+Solution: upgrade to ~> 2.3.18, ~> 3.1.12, >= 3.2.13
+
+Name: actionpack
+Version: 3.2.10
+Advisory: OSVDB-91454
+Criticality: Medium
+URL: http://osvdb.org/show/osvdb/91454
+Title: XSS Vulnerability in the `sanitize` helper of Ruby on Rails
+Solution: upgrade to ~> 2.3.18, ~> 3.1.12, >= 3.2.13
+
+Name: actionpack
+Version: 3.2.10
+Advisory: OSVDB-89026
 Criticality: High
 URL: http://osvdb.org/show/osvdb/89026
 Title: Ruby on Rails params_parser.rb Action Pack Type Casting Parameter Parsing Remote Code Execution
@@ -31,7 +47,15 @@ Solution: upgrade to ~> 2.3.15, ~> 3.0.19, ~> 3.1.10, >= 3.2.11
 
 Name: activerecord
 Version: 3.2.10
-Advisory: CVE-2013-0276
+Advisory: OSVDB-91453
+Criticality: High
+URL: http://osvdb.org/show/osvdb/91453
+Title: Symbol DoS vulnerability in Active Record
+Solution: upgrade to ~> 2.3.18, ~> 3.1.12, >= 3.2.13
+
+Name: activerecord
+Version: 3.2.10
+Advisory: OSVDB-90072
 Criticality: Medium
 URL: http://direct.osvdb.org/show/osvdb/90072
 Title: Ruby on Rails Active Record attr_protected Method Bypass
@@ -39,11 +63,19 @@ Solution: upgrade to ~> 2.3.17, ~> 3.1.11, >= 3.2.12
 
 Name: activerecord
 Version: 3.2.10
-Advisory: CVE-2013-0155
+Advisory: OSVDB-89025
 Criticality: High
 URL: http://osvdb.org/show/osvdb/89025
 Title: Ruby on Rails Active Record JSON Parameter Parsing Query Bypass
 Solution: upgrade to ~> 2.3.16, ~> 3.0.19, ~> 3.1.10, >= 3.2.11
+
+Name: activesupport
+Version: 3.2.10
+Advisory: OSVDB-91451
+Criticality: High
+URL: http://www.osvdb.org/show/osvdb/91451
+Title: XML Parsing Vulnerability affecting JRuby users
+Solution: upgrade to ~> 3.1.12, >= 3.2.13
 
 Unpatched versions found!
       }.strip.split "\n\n"
@@ -57,7 +89,7 @@ Unpatched versions found!
     let(:directory) { File.join('spec','bundle',bundle) }
 
     let(:command) do
-      File.expand_path(File.join(File.dirname(__FILE__),'..','bin','bundle-audit -i CVE-2013-0156'))
+      File.expand_path(File.join(File.dirname(__FILE__),'..','bin','bundle-audit -i OSVDB-89026'))
     end
 
     subject do
@@ -65,7 +97,7 @@ Unpatched versions found!
     end
 
     it "should not print advisory information for ignored gem" do
-      subject.should_not include("CVE-2013-0156")
+      subject.should_not include("OSVDB-89026")
     end
   end
 
