ca2ae509b319d770061f7d72f241b41762391f27
Postmodern <postmodern.mod3@gmail.com>
2013-08-26 17:44:24 -0700

    Updated Advisory specs to use the new OSVDB advisory files.

--- spec/advisory_spec.rb
+++ spec/advisory_spec.rb
@@ -5,15 +5,15 @@ require 'bundler/audit/advisory'
 describe Bundler::Audit::Advisory do
   let(:root) { Bundler::Audit::Database::PATH }
   let(:gem)  { 'actionpack' }
-  let(:cve)  { '2013-0156' }
-  let(:path) { File.join(root,gem,"#{cve}.yml") }
+  let(:id)   { 'OSVDB-84243' }
+  let(:path) { File.join(root,gem,"#{id}.yml") }
 
   describe "load" do
     let(:data) { YAML.load_file(path) }
 
     subject { described_class.load(path) }
 
-    its(:cve)         { should == cve                 }
+    its(:id)          { should == id                  }
     its(:url)         { should == data['url']         }
     its(:title)       { should == data['title']       }
     its(:cvss_v2)     { should == data['cvss_v2']     }
@@ -55,9 +55,6 @@ describe Bundler::Audit::Advisory do
   end
 
   describe "#unaffected?" do
-    let(:gem)  { 'activerecord' }
-    let(:cve)  { '2013-0155'    }
-
     subject { described_class.load(path) }
 
     context "when passed a version that matches one unaffected version" do
@@ -89,7 +86,7 @@ describe Bundler::Audit::Advisory do
     end
 
     context "when passed a version that matches no patched version" do
-      let(:version) { Gem::Version.new('3.1.9') }
+      let(:version) { Gem::Version.new('2.9.0') }
 
       it "should return false" do
         subject.patched?(version).should be_false
@@ -109,20 +106,17 @@ describe Bundler::Audit::Advisory do
     end
 
     context "when passed a version that matches no patched version" do
-      let(:version) { Gem::Version.new('3.1.9') }
+      let(:version) { Gem::Version.new('2.9.0') }
 
       it "should return true" do
         subject.vulnerable?(version).should be_true
       end
 
       context "when unaffected_versions is not empty" do
-        let(:gem)  { 'activerecord' }
-        let(:cve)  { '2013-0155'    }
-
         subject { described_class.load(path) }
      
         context "when passed a version that matches one unaffected version" do
-          let(:version) { Gem::Version.new('2.3.15') }
+          let(:version) { Gem::Version.new('2.3.12') }
 
           it "should return false" do
             subject.vulnerable?(version).should be_false
