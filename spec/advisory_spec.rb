175ac152999755028db124ee10e997d82b411358
Jon Frisby <jfrisby@mrjoy.com>
2013-12-07 22:42:58 -0800

    DRY up a bit.
    While this code is more complex, it doesn't duplicate logic from
    Bundler::Audit::Advisory.

--- spec/advisory_spec.rb
+++ spec/advisory_spec.rb
@@ -8,13 +8,33 @@ describe Bundler::Audit::Advisory do
   let(:id)   { 'OSVDB-84243' }
   let(:path) { File.join(root,'gems',gem,"#{id}.yml") }
   let(:an_unaffected_version) do
-    YAML.
-      load(File.read(path))['unaffected_versions'].
-      map { |item| item.split(/\s*,\s*/) }.
+    Bundler::Audit::Advisory.
+      # Parse the YAML-based advisory format into something coherent...
+      load(path).
+      # Retrieve the list of rules specifying what versions are NOT affected
+      # by the advisory...
+      unaffected_versions.
+      # For all the rules, get the individual constraints out and see if we can
+      # find a suitable one...
+      map do |version_rule|
+        version_rule.
+          requirements.
+          # We only want constraints where the version number specified is
+          # one of the unaffected version.  I.E. we don't want ">", "<", or if
+          # such a thing exists, "!=" constraints.
+          select do |(constraint, gem_version)|
+            ['~>', '>=', '=', '<='].include?(constraint)
+          end.
+          # Now, fetch just the version component, which is a Gem::Version,
+          # and extract the string representation of the version.
+          map do |(constraint, gem_version)|
+            gem_version.version
+          end
+      end.
+      # Now flatten out list-of-lists, and retrieve the first eligible version,
+      # as frankly, anything from the list will do.
       flatten.
-      select { |ver| ver =~ /^(~>|>=|=|<=)/ }.
-      first.
-      sub(/^.*?(~>|>=|=|<=)\s+/, '')
+      first
   end
 
   describe "load" do
