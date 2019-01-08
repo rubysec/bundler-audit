2bfbe3775532cec859985347bd55c1d6ae725c0b
Postmodern <postmodern.mod3@gmail.com>
2019-01-07 17:31:53 -0800

    Also support four part version numbers in the Version: regexp line.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -21,7 +21,7 @@ describe "CLI" do
 
     it "should print advisory information for the vulnerable gems" do
       advisory_pattern = %r{(Name: [^\n]+
-Version: \d+\.\d+\.\d+
+Version: \d+\.\d+\.\d+(\.\d+)?
 Advisory: CVE-[0-9]{4}-[0-9]{4}
 Criticality: (High|Medium|Low|Unknown)
 URL: https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
