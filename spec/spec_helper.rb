d7641dd07a4eeaa710b21922f74580c2ae95d7b1
Postmodern <postmodern.mod3@gmail.com>
2020-12-12 16:59:55 -0800

    No longer run commands in a clean environment.
    * This prevents bundler-audit's dependencies (thor and bundler) from being loaded.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,11 +20,9 @@ end
 
 module Helpers
   def sh(command, options={})
-    Bundler.with_clean_env do
-      result = `#{command} 2>&1`
-      raise "FAILED #{command}\n#{result}" if $?.success? == !!options[:fail]
-      result
-    end
+    result = `#{command} 2>&1`
+    raise "FAILED #{command}\n#{result}" if $?.success? == !!options[:fail]
+    result
   end
 
   def decolorize(string)
