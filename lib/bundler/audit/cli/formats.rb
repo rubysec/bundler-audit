d3a9abbe233447539ecb056c5496ece6566e6987
Postmodern <postmodern.mod3@gmail.com>
2020-12-16 23:22:43 -0800

    Added top-level documentation to Bundler::Audit::CLI::Formats explaining the API.

--- lib/bundler/audit/cli/formats.rb
+++ lib/bundler/audit/cli/formats.rb
@@ -20,6 +20,56 @@ require 'thor'
 module Bundler
   module Audit
     class CLI < ::Thor
+      #
+      # {Bundler::Audit::CLI} supports outputting it's audit results as
+      # {Bundler::Audit::CLI::Formats::Text text} or
+      # {Bundler::Audit::CLI::Formats::JSON JSON}, by default.
+      #
+      # ## API
+      #
+      # {Bundler::Audit::CLI} formats are really just modules defined under
+      # {Bundler::Audit::CLI::Formats}, that define a `#print_report` entry
+      # method. When the `#print_report` method is called it will be passed a
+      # {Bundler::Audit::Report report} object and an optional `output`
+      # stream, which may be either `$stdout` or a `File` object.
+      #
+      # {Bundler::Audit::CLI} will load a format by first calling
+      # {Formats.load}, which attempts to require the
+      # `bundler/audit/cli/formats/#{format}.rb` file, then gets the registered
+      # format module using {Formats.[]}.
+      # If the format module has been successfully loaded, it will be extended
+      # into the {Bundler::Audit::CLI} instance method access to
+      # {Bundler::Audit::CLI}'s instance methods.
+      #
+      # ## Custom Formats
+      #
+      # To define a custom format, it...
+      #
+      # * MUST be defined in a file in a
+      # `lib/bundler/audit/cli/formats/` directory.
+      # * MUST define a `print_report(report,output=$stdout)` instance method.
+      # * MUST register themselves by calling {Formats.register} at the end
+      #   of the file.
+      #
+      # ### Example
+      #
+      #     # lib/bundler/audit/cli/formats/my_format.rb
+      #     module Bundler
+      #       module Audit
+      #         class CLI < ::Thor
+      #           module Formats
+      #             module MyFormat
+      #               def print_report(report,output=$stdout)
+      #                 # ...
+      #               end
+      #             end
+      #
+      #             Formats.register :my_format, MyFormat
+      #           end
+      #         end
+      #       end
+      #     end
+      #
       module Formats
         class FormatNotFound < RuntimeError
         end
@@ -63,7 +113,9 @@ module Bundler
         end
 
         #
-        # Loads the format with the given name.
+        # Loads the format with the given name by attempting to require
+        # `bundler/audit/cli/formats/#{name}` and returning the registered
+        # format using {[]}.
         #
         # @param [#to_s] name
         #
