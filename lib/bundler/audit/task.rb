ccc582e54742d0d8806939e7c816f1fa87cbab56
Postmodern <postmodern.mod3@gmail.com>
2022-05-19 12:19:50 -0700

    Changed `Bundler::Audit::Task` to exit with `bundler-audit`'s error code (#345).
    * This will ensure that the rake task will fail CI if any
      vulnerabilities are found, or if `bundler-audit update` fails for some
      reason.

--- lib/bundler/audit/task.rb
+++ lib/bundler/audit/task.rb
@@ -23,12 +23,12 @@ module Bundler
           namespace :audit do
             desc 'Checks the Gemfile.lock for insecure dependencies'
             task :check do
-              system 'bundler-audit', 'check'
+              bundler_audit 'check'
             end
 
             desc 'Updates the bundler-audit vulnerability database'
             task :update do
-              system 'bundler-audit', 'update'
+              bundler_audit 'update'
             end
           end
 
@@ -38,6 +38,35 @@ module Bundler
         task 'bundler:audit'        => 'bundle:audit'
         task 'bundler:audit:check'  => 'bundle:audit:check'
         task 'bundler:audit:update' => 'bundle:audit:update'
+      end
+
+      private
+
+      #
+      # Runs the `bundler-audit` command with the additional arguments.
+      #
+      # @param [Array<String>] arguments
+      #   Additional command-line arguments for `bundler-audit`.
+      #
+      # @return [true]
+      #   The `bundler-audit` command successfully exited.
+      #
+      # @raise [RuntimeError]
+      #   The `bundler-audit` command could not be executed or was not found.
+      #
+      # @note
+      #   If the `bundler-audit` command exits with an error, the rake task
+      #   will also exit with the same error code.
+      #
+      def bundler_audit(*arguments)
+        case system('bundler-audit',*arguments)
+        when false
+          exit $?.exitstatus || 1
+        when nil
+          fail("bundler-audit could not be executed")
+        else
+          return true
+        end
       end
     end
   end
