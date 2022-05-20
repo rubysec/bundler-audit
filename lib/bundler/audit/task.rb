f519e5728a57d47cb028e2790e8c993295211f99
Postmodern <postmodern.mod3@gmail.com>
2022-05-19 19:57:01 -0700

    Expose `Task#bundler_audit`, `#check`, and `#update` for testability.

--- lib/bundler/audit/task.rb
+++ lib/bundler/audit/task.rb
@@ -16,33 +16,6 @@ module Bundler
         define
       end
 
-      protected
-
-      #
-      # Defines the `bundle:audit` and `bundle:audit:update` task.
-      #
-      def define
-        namespace :bundle do
-          namespace :audit do
-            desc 'Checks the Gemfile.lock for insecure dependencies'
-            task :check do
-              bundler_audit 'check'
-            end
-
-            desc 'Updates the bundler-audit vulnerability database'
-            task :update do
-              bundler_audit 'update'
-            end
-          end
-
-          task :audit => 'audit:check'
-        end
-
-        task 'bundler:audit'        => 'bundle:audit'
-        task 'bundler:audit:check'  => 'bundle:audit:check'
-        task 'bundler:audit:update' => 'bundle:audit:update'
-      end
-
       #
       # Runs the `bundler-audit` command with the additional arguments.
       #
@@ -68,6 +41,70 @@ module Bundler
         else
           return true
         end
+      end
+
+      #
+      # Runs the `bundle-audit check` command.
+      #
+      # @return [true]
+      #   The `bundler-audit` command successfully exited.
+      #
+      # @raise [CommandNotFound]
+      #   The `bundler-audit` command could not be executed or was not found.
+      #
+      # @note
+      #   If the `bundler-audit` command exits with an error, the rake task
+      #   will also exit with the same error code.
+      #
+      # @api private
+      #
+      def check
+        bundler_audit 'check'
+      end
+
+      #
+      # Runs the `bundle-audit update` command.
+      #
+      # @return [true]
+      #   The `bundler-audit` command successfully exited.
+      #
+      # @raise [CommandNotFound]
+      #   The `bundler-audit` command could not be executed or was not found.
+      #
+      # @note
+      #   If the `bundler-audit` command exits with an error, the rake task
+      #   will also exit with the same error code.
+      #
+      # @api private
+      #
+      def update
+        bundler_audit 'update'
+      end
+
+      protected
+
+      #
+      # Defines the `bundle:audit` and `bundle:audit:update` task.
+      #
+      def define
+        namespace :bundle do
+          namespace :audit do
+            desc 'Checks the Gemfile.lock for insecure dependencies'
+            task :check do
+              check
+            end
+
+            desc 'Updates the bundler-audit vulnerability database'
+            task :update do
+            end
+          end
+
+          task :audit => 'audit:check'
+        end
+
+        task 'bundler:audit'        => 'bundle:audit'
+        task 'bundler:audit:check'  => 'bundle:audit:check'
+        task 'bundler:audit:update' => 'bundle:audit:update'
       end
     end
   end
